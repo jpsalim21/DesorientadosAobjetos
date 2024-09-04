dbc272fe9bea5c1a728ec70ef7c8c57ea71f4f4a
mateuslfelicio <mateuslfvrb@hotmail.com>
2024-09-04 10:42:33 -0300

    Adiciona gerenciador de arquivos e modifica persistencia do jogador

---
+++ src/main/java/Persistencias/Arquivo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistencias;
+
+/**
+ *
+ * @author mateu
+ */
+public class Arquivo {
+    public static String le(String caminhoArq){
+        return "";
+    }
+    
+    public static void save(String camihoArq, String conteudo){
+        
+    }
+}
--- src/main/java/Persistencias/JogadorPersistencia.java
+++ src/main/java/Persistencias/JogadorPersistencia.java
@@ -2,10 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package Persistencias;
 import Usuarios.Jogador;
 import java.io.File;
 import java.util.*;
+import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
 /**
  *
  * @author mateu
@@ -16,12 +20,29 @@ public class JogadorPersistencia implements Persistencia<Jogador>{
     
     @Override
     public void save(List<Jogador> jogadores){
+        Gson gson = new Gson();
+        String json = gson.toJson(jogadores);
         
+        File diretorio = new File(DIRECTORY);
+        if(!diretorio.exists())
+            diretorio.mkdirs();
+        
+        Arquivo.save(PATH, json);
     }
     
     @Override
     public List<Jogador> findAll(){
+        Gson gson = new Gson();
+        String json = Arquivo.le(PATH);
         List<Jogador> jogadores = new ArrayList<>();
-       return jogadores; 
+        if(!json.trim().equals(" ")){
+            Type tipoLista = new TypeToken<List<Jogador>>(){}.getType();
+            jogadores = gson.fromJson(json, tipoLista);
+            
+            if(jogadores == null)
+                jogadores = new ArrayList<>();
+        }
+            
+        return jogadores; 
     }
 }
