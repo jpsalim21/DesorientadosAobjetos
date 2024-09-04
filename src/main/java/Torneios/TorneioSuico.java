4bd84c2c8853450cf6333c31dd05d296aaba50c2
Joao Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-09-04 16:29:48 -0300

    Cria classe do torneio suiço

---
+++ src/main/java/Torneios/TorneioSuico.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Torneios;
+
+import Usuarios.Jogador;
+import java.util.List;
+
+/**
+ *
+ * @author ice
+ */
+public class TorneioSuico extends Torneio{
+    
+    List<Jogador> classificacaoAtual;
+    
+    public TorneioSuico(String nome) {
+        super(nome);
+    }
+    
+    
+    
+}
