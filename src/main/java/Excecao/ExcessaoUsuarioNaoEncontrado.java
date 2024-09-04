267f4766c0e02364cc574b8d7adf1b7a0938c322
mateuslfelicio <mateuslfvrb@hotmail.com>
2024-09-04 17:42:52 -0300

    Cria excessao de usuario

---
+++ src/main/java/Excecao/ExcessaoUsuarioNaoEncontrado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excecao;
+
+/**
+ *
+ * @author mateu
+ */
+public class ExcessaoUsuarioNaoEncontrado extends Exception{
+
+    public ExcessaoUsuarioNaoEncontrado() {
+        super("Usuario não encontrado!");
+    }
+}
