f1eca6f04dd8a868e8175a667bee4a243472fd4d
mateuslfelicio <mateuslfvrb@hotmail.com>
2024-09-04 11:00:26 -0300

    Modifica gerenciador de Arquivos

--- src/main/java/Persistencias/Arquivo.java
+++ src/main/java/Persistencias/Arquivo.java
@@ -4,16 +4,31 @@
  */
 package Persistencias;
 
+import java.io.*;
+
 /**
  *
  * @author mateu
  */
 public class Arquivo {
     public static String le(String caminhoArq){
-        return "";
+        StringBuilder conteudo = new StringBuilder();
+        
+        try (BufferedReader leitor = new BufferedReader(new FileReader(caminhoArq))){
+            String linha;
+            while((linha = leitor.readLine()) != null)
+                conteudo.append(linha).append("\n");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return conteudo.toString();
     }
     
     public static void save(String camihoArq, String conteudo){
-        
+        try(BufferedWriter escritor = new BufferedWriter(new FileWriter(camihoArq,false))){
+            escritor.write(conteudo);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
