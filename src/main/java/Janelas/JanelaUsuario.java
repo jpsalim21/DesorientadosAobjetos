b8320c5a59d4e25fbe89f36c2a7914db319b64f7
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-09-12 11:09:27 -0300

    Arruma janela do usuário

--- src/main/java/Janelas/JanelaUsuario.java
+++ src/main/java/Janelas/JanelaUsuario.java
@@ -65,7 +65,7 @@ public class JanelaUsuario {
         
         JPanel painelPrincipal = new JPanel();
         painelPrincipal.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        painelPrincipal.setBorder(BorderFactory.createTitledBorder("Tela de jogador"));
+        painelPrincipal.setBorder(BorderFactory.createTitledBorder("Tela do jogador"));
         painelPrincipal.setLayout(new BorderLayout());
         
         JPanel painelInformacoes = new JPanel();
@@ -73,23 +73,42 @@ public class JanelaUsuario {
         painelInformacoes.setBorder(BorderFactory.createTitledBorder("Suas informações"));
         painelInformacoes.setPreferredSize(new Dimension(WIDTH/8, HEIGHT));
         
-        painelInformacoesAux.setLayout(new GridLayout(0, 1, H_GAP, V_GAP));
         JLabel nome = new JLabel("Nome: " + jogadorLogado.getNome());
         JLabel vitorias = new JLabel("Vitórias: " + jogadorLogado.getVitorias());
         JLabel empates = new JLabel("Empates: " + jogadorLogado.getEmpates());
         JLabel derrotas = new JLabel("Derrotas: " + jogadorLogado.getDerrotas());
         
+        painelInformacoesAux.setLayout(new GridLayout(0, 1, H_GAP, V_GAP));
         painelInformacoesAux.add(nome);
         painelInformacoesAux.add(vitorias);
         painelInformacoesAux.add(empates);
         painelInformacoesAux.add(derrotas);
         
+        
+        JPanel painelTorneios = new JPanel();
+        painelTorneios.setBorder(BorderFactory.createTitledBorder("Seus torneios"));
+        JPanel torneiosList = new JPanel();
+        painelTorneios.setLayout(new BorderLayout());
+        JPanel botoesPainelTorneios = new JPanel();
         torneiosEntrados = new JList<>(model);
-        painelPrincipal.add(new JScrollPane(torneiosEntrados), BorderLayout.CENTER);
+        JScrollPane painelScrollTorneios = new JScrollPane(torneiosEntrados);
+        painelScrollTorneios.setPreferredSize(new Dimension(WIDTH * 5/8, HEIGHT*3/5));
+        torneiosList.add(painelScrollTorneios, BorderLayout.NORTH);
+        
+        JButton btnAcessar = new JButton("Acessar Torneio");
+        JButton btnDeslogar = new JButton("Deslogar da Conta");
+        botoesPainelTorneios.add(btnAcessar);
+        botoesPainelTorneios.add(btnDeslogar);
+        
+        painelTorneios.add(torneiosList, BorderLayout.NORTH);
+        painelTorneios.add(botoesPainelTorneios, BorderLayout.SOUTH);
+        //painelPrincipal.add(new JScrollPane(torneiosEntrados), BorderLayout.CENTER);
         
         
         painelInformacoes.add(painelInformacoesAux, BorderLayout.NORTH);
+        
         painelPrincipal.add(painelInformacoes, BorderLayout.WEST);
+        painelPrincipal.add(painelTorneios, BorderLayout.CENTER);
         janela.add(painelPrincipal);
         janela.pack();
     }
